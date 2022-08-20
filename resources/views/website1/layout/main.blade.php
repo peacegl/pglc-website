889ccc3f13fb047b55c1f07a9cb2da86d5a428c5
arahimfahim40 <57112494+arahimfahim40@users.noreply.github.com>
2022-08-21 00:51:19 +0300

    contact changes

--- resources/views/website1/layout/main.blade.php
+++ resources/views/website1/layout/main.blade.php
@@ -246,6 +246,44 @@
     <script src="{{asset('asset/js/bootsnav.js')}}"></script>
     <script src="{{asset('asset/js/main.js')}}"></script>
 
+
+        <!-- chat bot whatsapp to Faisal whatsapp -->
+        <script>
+    var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?92638';
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = url;
+    var options = {
+  "enabled":true,
+  "chatButtonSetting":{
+      "backgroundColor":"#4dc247",
+      "ctaText":"Message Us",
+      "borderRadius":"25",
+      "marginLeft":"0",
+      "marginBottom":"50",
+      "marginRight":"50",
+      "position":"right"
+  },
+  "brandSetting":{
+      "brandName":"Peace Global Logistics",
+      "brandSubTitle":"Typically replies within a day",
+      "brandImg":"https://cdn.clare.ai/wati/images/WATI_logo_square_2.png",
+      "welcomeText":"Hi there!\nHow can I help you?",
+      "messageText":"",
+      "backgroundColor":"#0a5f54",
+      "ctaText":"Start Chat",
+      "borderRadius":"25",
+      "autoShow":false,
+      "phoneNumber":"971565929272"
+  }
+};
+    s.onload = function() {
+        CreateWhatsappChatWidget(options);
+    };
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+</script>
 </body>
 
 
